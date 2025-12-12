92cb6e17b7a1388973dabfc58fc05936604305f8
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-12 14:52:53 -0500

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 export default function Footer() {
   return (
     <footer>
-      <p>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+      {/* <p>
         <a href="https://pngtree.com">icons & images from pngtree.com</a>
-      </p>
+      </p> */}
     </footer>
   );
 }
