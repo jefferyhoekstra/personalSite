f48c9a8ea283c2d2b09a2414e76063a297008a20
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-12 16:58:22 -0500

    1

--- src/components/Element1.jsx
+++ src/components/Element1.jsx
@@ -1,14 +1,41 @@
 import img1 from "../assets/img2.png";
 import bg from "../assets/bg.png";
 
+import c__ from "../assets/lang/c++.svg";
+import react from "../assets/lang/react.svg";
+import csharp from "../assets/lang/c-sharp.png";
+import css3 from "../assets/lang/css-3.png";
+import html5 from "../assets/lang/html-5.png";
+import java from "../assets/lang/java.png";
+import js from "../assets/lang/js.png";
+import python from "../assets/lang/python.png";
+import c from "../assets/lang/c.png";
+
 export default function Element1() {
   return (
     <div className="bg">
       {/* <img src={bg} alt="" /> */}
       <div className="element1">
-        <h1>Jeffery Hoekstra</h1>
-        <h2>👽 Artist & Developer 🛸</h2>
-        <img src={img1} alt="" />
+        <div className="text-section">
+          <h1>Jeffery Hoekstra</h1>
+          <h2>Hardware & Software Developer</h2>
+          <div className="icon-section">
+            <img src={c} alt="" width="40px" />
+            <img src={c__} alt="" width="40px" />
+            <img src={csharp} alt="" width="40px" />
+            <img src={css3} alt="" width="40px" />
+            <img src={html5} alt="" width="40px" />
+            <img src={java} alt="" width="40px" />
+            <img src={js} alt="" width="40px" />
+            <img src={react} alt="" width="40px" />
+            {/* <img src={python} alt="" width="40px" /> */}
+            {/* <img src={react} alt="" width="45px" />
+            <img src={react} alt="" width="45px" /> */}
+          </div>
+        </div>
+        <div className="image-section">
+          <img src={img1} alt="" />
+        </div>
       </div>
     </div>
   );
