4b7d7fe4944bf828139b5a8b2198b9fa9605154b
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-12 08:20:00 -0500

    1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        <a href="https://pngtree.com">Icons & images from pngtree.com/</a>
+      </p>
+    </footer>
+  );
+}
