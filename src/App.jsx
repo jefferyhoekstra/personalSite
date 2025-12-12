0cef73f60580b39d3ae8cd605740cf7cc47d86a6
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-12 08:28:37 -0500

    5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Footer from "./components/Footer";
+import Body from "./components/Body";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-
+      <Body />
       <Footer />
     </>
   );
---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.png";
+
+export default function Footer() {
+  return (
+    <div className="bodyImage">
+      <img src={logo} alt="" />
+    </div>
+  );
+}
