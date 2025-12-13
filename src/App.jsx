f10990c4fb9c61184cf870dc5a349754d15d0043
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-12 20:39:24 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Element1 from "./components/Element1";
+import WIP from "./components/WIP";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -32,6 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
+      <WIP />
 
       <Header />
       <Element1 />
---
+++ src/components/WIP.jsx
@@ -0,0 +1,7 @@
+export default function WIP() {
+  return (
+    <div className="WIP">
+      <p>Website currently a Work in Progress</p>
+    </div>
+  );
+}
