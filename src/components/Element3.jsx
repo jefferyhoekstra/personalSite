03077cc60601c00fb01bf78ca3c9c40dd6e8886a
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-13 23:19:05 -0500

    Update Element3.jsx

--- src/components/Element3.jsx
+++ src/components/Element3.jsx
@@ -1,7 +1,10 @@
 export default function Element3() {
   return (
     <div className="element3">
-      <h3>Contact Me</h3>
+      <h3>
+        If you’d like to talk about a project, idea, or opportunity, feel free
+        to contact me
+      </h3>
       <p>
         E-mail:{" "}
         <a href="mailto:jefferyhoekstra@hotmail.com">
