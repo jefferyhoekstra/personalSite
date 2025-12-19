a369fb10d109ddaf2dcaccafc5807bf5b082c5f6
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-18 23:22:36 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,14 +4,14 @@ export default function Projects() {
   return (
     <section className="Projects">
       <div className="title">
-        <h2>My Projects</h2>
+        <h3>My Projects</h3>
       </div>
       <a href="https://www.wavenet-ten.vercel.app">NetWave Blogsite </a>
       <div className="ProjectsList">
         <iframe
           src="https://wavenet-ten.vercel.app/"
           width="450"
-          height="650"
+          height="600"
           style={{ border: "2px solid #5448d6ff", borderRadius: "8px" }}
           loading="lazy"
         ></iframe>
