0eaadf8cc6ecdbf3aee58a4646f25cc235f6dfd2
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-18 23:24:11 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,9 @@ export default function Projects() {
       <div className="title">
         <h3>My Projects</h3>
       </div>
-      <a href="https://www.wavenet-ten.vercel.app">NetWave Blogsite </a>
+      <a href="https://wavenet-27w3b6ufr-jefferyhoekstras-projects.vercel.app">
+        NetWave Blogsite{" "}
+      </a>
       <div className="ProjectsList">
         <iframe
           src="https://wavenet-ten.vercel.app/"
