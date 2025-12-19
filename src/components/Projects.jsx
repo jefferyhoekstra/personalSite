4af19dd24b242ee0dbf0b54c83900bf682aae819
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-18 23:31:48 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,12 @@
 import "./css/Projects.css";
+import arrowDown from "../assets/arrowDown.png";
 
 export default function Projects() {
   return (
     <section className="Projects">
       <div className="title">
         <h3>My Projects</h3>
+        <img src={arrowDown} alt="" width="54px" />
       </div>
       <a href="https://wavenet-27w3b6ufr-jefferyhoekstras-projects.vercel.app">
         NetWave Blogsite{" "}
