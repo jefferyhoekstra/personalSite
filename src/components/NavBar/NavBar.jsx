1b4136505f406f88be8e8ae0bdbe23cd5998eee0
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-24 14:39:37 -0500

    Added hamburger menu for About

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 /// COMPONENTS
 
+import { useState } from "react";
+
 /// CSS
 import "./navBar.css";
 
@@ -10,6 +12,9 @@ import contactIcon from "/src/assets/images/NavBar/contactIcon.svg";
 
 /// FUNCTION
 export default function NavBar() {
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+  const [suppressAboutHover, setSuppressAboutHover] = useState(false);
+
   return (
     <>
       <div className="navBar">
@@ -27,18 +32,70 @@ export default function NavBar() {
             </a>
           </li>
           <li>
-            <a
-              href="#about"
-              onClick={(e) => {
-                e.preventDefault();
-                document
-                  .querySelector(".element1")
-                  ?.scrollIntoView({ behavior: "smooth" });
+            <div
+              className={
+                suppressAboutHover
+                  ? "navBar__about navBar__about--suppressHover"
+                  : "navBar__about"
+              }
+              onMouseEnter={() => {
+                setSuppressAboutHover(false);
+              }}
+              onMouseLeave={() => {
+                setIsAboutOpen(false);
               }}
             >
-              <img src={aboutIcon} alt="" />
-              About
-            </a>
+              <a
+                href="#about"
+                aria-haspopup="menu"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSuppressAboutHover(false);
+                  setIsAboutOpen((open) => !open);
+                }}
+              >
+                <img src={aboutIcon} alt="" />
+                About
+              </a>
+
+              <div
+                className={
+                  isAboutOpen
+                    ? "navBar__aboutMenu navBar__aboutMenu--open"
+                    : "navBar__aboutMenu"
+                }
+                role="menu"
+              >
+                <a
+                  href="#bio"
+                  role="menuitem"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsAboutOpen(false);
+                    setSuppressAboutHover(true);
+                    document
+                      .querySelector(".element1")
+                      ?.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Bio
+                </a>
+                <a
+                  href="#projects"
+                  role="menuitem"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsAboutOpen(false);
+                    setSuppressAboutHover(true);
+                    document
+                      .querySelector(".projects")
+                      ?.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Projects
+                </a>
+              </div>
+            </div>
           </li>
           <li>
             <a
