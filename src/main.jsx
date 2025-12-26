2db7047d971df671b9dae27f90be833624e9c210
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-25 21:31:09 -0500

    d

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-function setAppViewportHeight() {
-  // Use innerHeight to avoid mobile URL-bar vh jumps.
-  document.documentElement.style.setProperty(
-    "--app-viewport-height",
-    `${window.innerHeight}px`
-  );
-}
-
-setAppViewportHeight();
-window.addEventListener("resize", setAppViewportHeight, { passive: true });
-window.addEventListener("orientationchange", setAppViewportHeight, {
-  passive: true,
-});
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
