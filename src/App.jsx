ea41bc410b01a92a31a462ffc7800e635930c092
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-26 05:16:45 -0500

    reverted to only one bg jpeg

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,6 @@ import NavBar from "./components/NavBar/NavBar";
 import HeroImage from "./components/Hero/HeroImage";
 import Hero from "./components/Hero/Hero";
 
-import HeaderBackground from "./components/HeaderBackground/HeaderBackground";
-
-import bg1 from "./assets/images/Hero/bg1.jpg";
-import bg4 from "./assets/images/Hero/bg4.svg";
-import heroBackground from "./assets/images/Hero/heroBackground.jpg";
-
 import Element1 from "./components/Element1/Element1";
 import Element2 from "./components/Element2/Element2";
 import Element3 from "./components/Element3/Element3";
@@ -22,13 +16,10 @@ import Projects from "./components/Projects/Projects";
 import ContactForm from "./components/ContactForm/ContactForm";
 
 export default function App() {
-  const headerBackgrounds = [bg1, bg4, heroBackground];
-
   return (
     <>
       <div className="app">
         <header className="app_header">
-          <HeaderBackground images={headerBackgrounds} intervalMs={8000} />
           <HeroImage />
           <Hero />
         </header>
